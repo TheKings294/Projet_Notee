487a026f9538713dda1014978e339f420520c841
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2024-09-26 16:46:33 +0200

    Ajout d'un ASCII art

--- main.c
+++ main.c
@@ -6,6 +6,12 @@ int main(int argc, const char * argv[]) {
     srand(time(NULL));
     int random_number = rand() % 100 + 1;
     int nb_user;
+ 
+    printf(" M   M    A    GGGG  I  CCCC     N   N  U   U  M   M  BBBB  EEEEE  RRRR  \n");
+    printf(" MM MM   A A   G     I  C        NN  N  U   U  MM MM  B   B  E      R   R \n");
+    printf(" M M M  AAAAA  G  GG I  CCCC     N N N  U   U  M M M  BBBB   EEEEE  RRRR  \n");
+    printf(" M   M  A   A  G   G I  C        N  NN  U   U  M   M  B   B  E      R  R  \n");
+    printf(" M   M  A   A   GGGG I  CCCC     N   N   UUU   M   M  BBBB   EEEEE  R   R \n\n");
     
     while (random_number != nb_user) {
         nb_user = -1;
