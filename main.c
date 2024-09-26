52356a55db695729a482f0bc1ebad5c5e9e49847
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2024-09-26 16:29:53 +0200

    Amélioration du code pour tester si c'est un nombre

--- main.c
+++ main.c
@@ -7,18 +7,23 @@ int main(int argc, const char * argv[]) {
     int random_number = rand() % 100 + 1;
     int nb_user;
     
-    
     while (random_number != nb_user) {
+        nb_user = -1;
         printf("Donne un nombre entre 1 et 100 : ");
         scanf("%d", &nb_user);
         
-        if (nb_user != random_number) {
+        if ((nb_user != random_number) && (nb_user >= 1 && nb_user <= 100)) {
             if (nb_user < random_number) {
                 printf("C'est plus !\n");
             }
             else {
                 printf("C'est moins !\n");
-            }        }
+            }
+        }
+        else {
+            printf("Ton input n'est pas nombre\n");
+            exit(0);
+        }
     }
     printf("Bravo !\n");
     
